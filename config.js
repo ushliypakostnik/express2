657452d852608d45fedd7860a15e622aa9107126
levon <levon.gambaryan@gmail.com>
2019-07-02 23:02:13 +0300

    add proj with mongoose

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const development = {
   PORT: process.env.PORT || 8082,
   MEDIA_DIR: process.env.MEDIA_DIR || 'media',
   MEDIA_URL: process.env.MEDIA_URL || 'http://127.0.0.1:8082/media',
-  STATIC_SERVE: true,
+  STATIC_SERVE: false,
   CORS_ENABLED: true,
 };
 
